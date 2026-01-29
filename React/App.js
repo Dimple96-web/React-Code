01b79e596e75a2573602d9161ad882a93d4376ea
Dimple96-web <dimpledechamma96@gmail.com>
2026-01-29 11:03:20 +0530

    Episode-03

--- React/App.js
+++ React/App.js
@@ -1,32 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/* <div id="parent">
-  <div id="child">
-    <h1 id="heading">Im a h1 tag</h1>
-    <h2>Im a h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1 id="heading">Im a h1 tag</h1>
-    <h2>Im a h2 tag</h2>
-  </div>
-</div> */
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Im a h1 tag"),
-    React.createElement("h1", {}, "Im a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Im a h1 tag"),
-    React.createElement("h1", {}, "Im a h2 tag"),
-  ]),
-]);
+const TitleComponent = () => {
+  return <h2>Title Component</h2>;
+};
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React",
+const paragraph = <p>This is a paragraph</p>;
+
+const HeadingComponent = () => (
+  <div>
+    <h1>Heading Component</h1>
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    {TitleComponent()}
+    {paragraph}
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
---
+++ React/practice.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+/* <div id="parent">
+  <div id="child">
+    <h1 id="heading">Im a h1 tag</h1>
+    <h2>Im a h2 tag</h2>
+  </div>
+  <div id="child2">
+    <h1 id="heading">Im a h1 tag</h1>
+    <h2>Im a h2 tag</h2>
+  </div>
+</div> */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Im a h1 tag"),
+    React.createElement("h1", {}, "Im a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "Im a h1 tag"),
+    React.createElement("h1", {}, "Im a h2 tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React",
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+// JSX Code
+const jsxHeading = <h1 id="heading">Hello World from JSX</h1>;
